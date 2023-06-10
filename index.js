af7e331215725cf30666a6354866ca800e2a830b
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-10 18:36:36 +0530

    💝 Queen Hentai 💝

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('💝 Queen Hentai 💝', {
+say('Queen Hentai', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By 💝 Dinuwa Official²⁰²³ 💝._`, {
+say(`'${name}' By Dinuwa Official²⁰²³._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
