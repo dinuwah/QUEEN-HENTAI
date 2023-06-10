3f349ecfe281da219bee74712881eed2c7d62d3f
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-06-10 07:47:26 +0530

    ABHISHEK-SER👻💫

--- plugins/GURU-toanime.js
+++ plugins/GURU-toanime.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ""
 if (!/image/g.test(mime)) throw '*Respond to a image*'
-m.reply('*This command can turn your photo into anime*')    
+m.reply('*ABHISHEK-SER*_Changing Your Image To Anime Please Wait_')    
 let data = await q.download?.()
 let image = await uploadImage(data)
 try {
@@ -18,7 +18,7 @@ try{
 let anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`
 await conn.sendFile(m.chat, anime3, 'error.jpg', null, m) 
 } catch (e) {
-throw '*Error check if the persons face is visible*'
+throw '*Error Check If The Persons Face Is Visible*'
 }}}}
 handler.help = ["toanime"]
 handler.tags = ["tools"]
