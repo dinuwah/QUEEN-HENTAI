13e3bdab44ac9aebbb60c41a724ad8b87534d976
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-11 12:46:08 +0530

    💝 Queen Hentai 💝

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -3,18 +3,18 @@ import { youtubeSearch } from '@bochilteam/scraper'
 import yts from 'yt-search'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
-    if (!text) throw `😑 Enter A Song Title\n\n*📌 Example*\n*${usedPrefix + command}* Heat Waves `
-    m.react('📀')
+    if (!text) throw `💝 Enter A Song \n\n*💝 Ex;*\n*${usedPrefix + command}* Sudu Nona`
+    m.react('💝')
     let result = await yts(text)
     let ytres = result.all
     let listSections = []
 	Object.values(ytres).map((v, index) => {
 	listSections.push([`${index}┃ ${v.title}`, [
-          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `▢ ⌚ *Duration:* ${v.timestamp}\n▢ 👀 *Views:* ${v.views}\n▢ 📌 *Títle* : ${v.title}\n▢ 📆 *Publiced:* ${v.ago}\n`],
-          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `▢ ⌚ *Duratión:* ${v.timestamp}\n▢ 👀 *Views:* ${v.views}\n▢ 📌 *Títle* : ${v.title}\n▢ 📆 *Publiced:* ${v.ago}\n`]
+          ['💝 MP3', `${usedPrefix}fgmp3 ${v.url}`, `▢ 💝 *Duration:* ${v.timestamp}\n▢ 💝 *Views:* ${v.views}\n▢ 💝 *Títle* : ${v.title}\n▢ 💝 *Publiced:* ${v.ago}\n`],
+          ['💝 MP4', `${usedPrefix}fgmp4 ${v.url}`, `▢ 💝 *Duratión:* ${v.timestamp}\n▢ 💝 *Views:* ${v.views}\n▢ 💝 *Títle* : ${v.title}\n▢ 💝 *Publiced:* ${v.ago}\n`]
         ]])
 	})
-	return conn.sendList(m.chat, '  ≡ *ABHI MUSIC*🔎', `\n 📀 Here a list of results from :\n *${text}*`, igfg, `Click `, listSections, m)
+	return conn.sendList(m.chat, '  ≡ *💝 Queen Hentai 💝 MUSIC*🔎', `\n 💝 Queen Hentai 💝 of results :\n *${text}*`, igfg, `💝Click💝 `, listSections, m)
 }
 handler.help = ['play2']
 handler.tags = ['dl']
