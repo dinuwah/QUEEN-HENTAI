edc3d30bdea021cc1ecf39c7cc5d09efc9418373
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-11 18:44:00 +0530

    💝 Queen Hentai 💝

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -2,14 +2,14 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
-≡ *DONATION*
-Send Redeem Code To This WhatsApp Number +919074692450 👻`
-let img = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'
+🇱🇰 *DONATE* 🇱🇰
+If you get more info of 💝 Queen Hentai 💝 Please Contact 94740804536 💰`
+let img = 'https://i.imgur.com/bFEC7lC.jpeg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
 handler.help = ['donate']
 handler.tags = ['main']
-handler.command = ['apoyar', 'donate', 'donar'] 
+handler.command = ['apoyar', 'donate', 'donasi'] 
 
 export default handler
