4c7bc0356b9dab2c562abd1d6151e9e3cec00fec
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-11 18:50:44 +0530

    💝 Queen Hentai 💝

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -14,8 +14,8 @@ reject(err)
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
-if (!args[0]) throw `📌 Example : ${usedPrefix + command} 😎+🤑`
-if (!text.includes('+')) throw  `✳️ Separate the emoji with a *+* \n\n📌 Example : \n*${usedPrefix + command}* 😎+🤑`
+if (!args[0]) throw `💰 Ex: ${usedPrefix + command} 🙄+😂`
+if (!text.includes('+')) throw  `🔊 Separate the 💝 Queen Hentai 💝 emoji with a *+* \n\n💰 Ex: \n*${usedPrefix + command}* 🙄+😂`
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
