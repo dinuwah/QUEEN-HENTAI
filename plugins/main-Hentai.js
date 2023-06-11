6217ecabedf118e779c8d5409a9ae23a0146d4e0
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-11 18:20:51 +0530

    💝 Queen Hentai 💝

---
+++ plugins/main-Hentai.js
@@ -0,0 +1,21 @@
+let handler = async m => m.reply(`
+
+   *💝 Queen Hentai 💝* WA Group
+🔊 Join Public Bot Group And Support
+https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S
+─────────────
+🔊 *Github*
+https://github.com/dinuwah/QUEEN-HENTAI
+ 
+🔊 *Contact*
+wa.me:94740804536
+
+🔊 *Instagram*
+https://instagram.com/Dinuwa Official²⁰²³
+
+`.trim())
+handler.help = ['gphentai']
+handler.tags = ['main']
+handler.command = ['groups', 'supporthentai', 'hentaigp', 'grouphentai', 'group'] 
+
+export default handler
