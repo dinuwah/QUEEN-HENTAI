7cb57884bea2dc2b85f9dd26a0f6e351eb893c5a
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-11 18:37:17 +0530

    💝 Queen Hentai 💝

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -3,11 +3,11 @@ import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
    /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
+    if (!/video|audio/.test(mime)) throw `🇱🇰 Mention to 💝 Queen Hentai 💝 a video or document for convert to mp3,with the command :\n\n*${usedPrefix + command}*`*/
     let media = await q.download?.()
-    if (!media) throw '❎ Failed to download media'
+    if (!media) throw '🇱🇰 Failed on 💝 Queen Hentai 💝 Server'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error converting'
+    if (!audio.data) throw '🇱🇰 Error 💝 Queen Hentai 💝 Server'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tomp3']
