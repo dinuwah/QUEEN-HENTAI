20f9b5e4c0b97eaabbae04cc4268ae008e38cf03
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-11 12:28:47 +0530

    💝 Queen Hentai 💝

--- plugins/dl-gdrive.js
+++ plugins/dl-gdrive.js
@@ -2,27 +2,27 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `✳️ Enter a Google Drive link`
+	if (!args[0]) throw `💝 Queen Hentai 💝, Enter a Google Drive link`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
 	 await m.reply(`
-≡ *Google Drive DL*
+≡ *💝 Queen Hentai 💝 Google Drive DOWNLOADER*
 
-▢ *Number:* ${res.fileName}
-▢ *Size:* ${res.fileSize}
-▢ *type:* ${res.mimetype}`)
+💝 *Number:* ${res.fileName}
+💝 *Size:* ${res.fileSize}
+💝 *type:* ${res.mimetype}`)
 		
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
    } catch {
-	m.reply('Error: Check the link or try another link') 
+	m.reply('💝 Error: Check the link or try another link 💝') 
   }
 }
 handler.help = ['gdrive']
 handler.tags = ['dl', 'prem']
 handler.command = ['gdrive']
-handler.diamond = true
-handler.premium = true
+handler.diamond = false
+handler.premium = false
 
 export default handler
