b59d47211bb1603daa0afe3062ed3d98e253ccc8
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-12 20:30:39 +0530

    💝 Queen Hentai 💝

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,13 +1,13 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 	
-let kickte = `✳️ Correct use of the command\n*${usedPrefix + command}* @tag`
+let kickte = `*Please boza ,Use Correct Command of the 💝 Queen Hentai 💝 WA BOT\n*${usedPrefix + command}* @tag`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ User kicked`) 
+m.reply(`😁 User kicked,*වැඩේ හරි*`) 
 
 }
 
