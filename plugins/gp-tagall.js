5b6cd9456e8d651d6f302bf9efc2633e11e2d7de
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-13 20:57:51 +0530

    *💝 Queen Hentai 💝*

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`🔊 Group : *${groupMetadata.subject}*\n🔊 Members : *${participants.length}*${text ? `\n🔊 Message : ${text}\n` : ''}\n┌───⊷ *Mention By 💝 Queen Hentai 💝*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ 💝 Queen Hentai 💝 ✪──', null, {
+    m.reply(`🔊 Group : *${groupMetadata.subject}*\n🔊 Members : *${participants.length}*${text ? `\n🔊 Message : ${text}\n` : ''}\n┌───⊷ *Mention By 💝 Queen Hentai 💝*\n` + users.map(v => '💝 @' + v.replace(/@.+/, '')).join`\n` + '\n└──*💝 Queen Hentai 💝*──', null, {
         mentions: users
     })
 }
