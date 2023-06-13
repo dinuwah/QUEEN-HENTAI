045c7bc345181cea99c84b3d692886a56d479874
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-13 12:37:57 +0530

    💝 Queen Hentai 💝

--- plugins/rg-sn.js
+++ plugins/rg-sn.js
@@ -3,7 +3,7 @@ import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
 m.reply(`
-▢ *seriel number* : ${sn}
+💝 *seriel number* : ${sn}
 `.trim())
 }
 handler.help = ['mysn']
