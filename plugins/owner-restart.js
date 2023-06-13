e4ebbbc0f79ff78836220f74514f06cc1a06c621
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-13 08:59:15 +0530

    💝 Queen Hentai 💝

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,14 +2,14 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🔄 Restarting Bot...\n Wait a moment')
+    await m.reply('🇱🇰 Restarting *💝 Queen Hentai 💝* WA BOT...\n Plzz waiting..ohoma poddk idm besikayo🤣')
     process.send('reset')
   } else throw 'eh'
 }
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['restart','reiniciar'] 
+handler.command = ['restart','hentairestart'] 
 
 handler.rowner = true
 
