b27f0c22c3e94e59d33e742c9be3d440afa5fa0d
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-13 08:44:12 +0530

    💝 Queen Hentai 💝

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -14,10 +14,10 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `*≡ Link Detected*
+        await conn.reply(m.chat, `*💝 Queen Hentai 💝Link Detected*
             
-We do not allow links from other groups 
-I'M Sorry 💝 Queen Hentai 💝 *@${m.sender.split('@')[0]}*  You Will Be Kicked Out Of The Group ${isBotAdmin ? '' : '\n\nIM Not An Admin So I Cannot 💝 Queen Hentai 💝 Expel You :"v'}`, null, { mentions: [m.sender] } )
+💝 Queen Hentai 💝Team doesn't allow links from other groups. 
+I'M Sorry, 💝 Queen Hentai 💝 WA BOT *@${m.sender.split('@')[0]}*  Will Be Kicked Out Of The Group 😁 ${isBotAdmin ? '' : '\n\n💝 Queen Hentai 💝 Not An Admin So I Cannot fuckoff 🤣 You :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
