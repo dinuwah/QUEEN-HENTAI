fb869dc8f8c57f315824850a0016cc29cf866806
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-13 08:47:13 +0530

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,6 +1,6 @@
 //import db from '../lib/database.js'
 
-import { execSync } from 'child_process'
+mport { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
