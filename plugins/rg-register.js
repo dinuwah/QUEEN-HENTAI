422e44e5093e32370d94dca70abb4441687d38c6
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-14 08:57:58 +0530

    💝 Queen Hentai 💝

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -6,13 +6,13 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
   if (user.registered === true) throw `💝 You are already registered with 💝 Queen Hentai 💝\n\n🇱🇰 Do you want to re-register to 💝 Queen Hentai 💝 ?🇱🇰\n\n 🇱🇰 Use this command to remove your register 🇱🇰\n*.unhentai* (Serial number)`
-  if (!Reg.test(text)) throw `🇱🇰 Format incorrect\n\n 🇱🇰 Use this command of 💝 Queen Hentai 💝: *.hentai name.age*\n💝Exemple : *.hentai* Dinuwa.16`
+  if (!Reg.test(text)) throw `🇱🇰 Format incorrect\n\n 🇱🇰 Use this command of 💝 Queen Hentai 💝: *.qhw name.age*\n💝Exemple : *.qhw* Dinuwa.16`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw '💝 The name cannot be empty'
   if (!age) throw '💝 age cannot be empty'
   if (name.length >= 30) throw '💝 The name is too long' 
   age = parseInt(age)
-  if (age > 100) throw '💝🏻 හම්මට නාකියා wants to play with 💝 Queen Hentai 💝 bot'
+  if (age > 100) throw '💝 හම්මට නාකියා wants to play with 💝 Queen Hentai 💝 bot'
   if (age < 5) throw '💝  there is a Carima podi eka '
   user.name = name.trim()
   user.age = age
@@ -34,7 +34,7 @@ ${sn}
 handler.help = ['reg'].map(v => v + ' <name.age>')
 handler.tags = ['rg']
 
-handler.command = ['verify', 'reg', 'register', 'hentai'] 
+handler.command = ['verify', 'reg', 'register', 'qhw'] 
 
 export default handler
 
