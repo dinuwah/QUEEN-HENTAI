7f3bfa72ca5f623ca3dd49351959baeee35a27de
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-14 09:05:26 +0530

    💝 Queen Hentai 💝

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,12 +2,12 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✔ *Enter 💝 Queen Hentai 💝 serial number*\nCheck your 💝 Queen Hentai 💝 serial number with the command...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `✔ *Enter 💝 Queen Hentai 💝 serial number*\nCheck your 💝 Queen Hentai 💝 serial number with the command...\n\n*${usedPrefix}dinuserial*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw '😤 *Incorrect 💝 Queen Hentai 💝 serial number*'
   user.registered = false
-  m.reply(`💭 Register ok by 💝 Queen Hentai 💝`)
+  m.reply(`💭 Un Register ok by 💝 Queen Hentai 💝`)
 }
 handler.help = ['unreg <Num Serie>'] 
 handler.tags = ['rg']
