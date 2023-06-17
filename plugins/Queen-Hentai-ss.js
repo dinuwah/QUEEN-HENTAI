edf548ed5ce307205f77b9de91032574f0df7e44
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-17 18:50:19 +0530

    💝 Queen Hentai 💝

---
+++ plugins/Queen-Hentai-ss.js
@@ -0,0 +1,10 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command, args }) => {
+if (!args[0]) return conn.reply(m.chat, '*🇱🇰 Bza give a link or url to 💝 Queen Hentai 💝*', m)
+let ss = await (await fetch(`🇱🇰 https://image.thum.io/get/fullpage/${args[0]} 🇱🇰`)).buffer()
+conn.sendFile(m.chat, ss, 'error.png', args[0], m)}
+handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
+handler.tags = ['tools']
+handler.command = /^ss(web)?f?$/i
+export default handler
+
