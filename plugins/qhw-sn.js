d9e262bdb0d11714fcbb1bda3f0d99de06dbac8f
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-06-17 13:34:16 +0530

    qhw-sn.js

--- plugins/qhw-sn.js
+++ plugins/qhw-sn.js
@@ -1,13 +1,1 @@
-import { createHash } from 'crypto'
-
-let handler = async function (m, { conn, text, usedPrefix }) {
-let sn = createHash('md5').update(m.sender).digest('hex')
-m.reply(`
-💝 *seriel number* : ${sn}
-`.trim())
-}
-handler.help = ['mysn']
-handler.tags = ['rg']
-handler.command = ['nserie', 'sn', 'dinuserial'] 
-handler.register = true
-export default handler
+const _0x425811=_0x3851;(function(_0x2516fa,_0x573ff6){const _0x47c227=_0x3851,_0x12b002=_0x2516fa();while(!![]){try{const _0x55a437=-parseInt(_0x47c227(0x1bb))/0x1+parseInt(_0x47c227(0x1bc))/0x2+parseInt(_0x47c227(0x1b4))/0x3*(-parseInt(_0x47c227(0x1ae))/0x4)+-parseInt(_0x47c227(0x1c0))/0x5*(parseInt(_0x47c227(0x1b1))/0x6)+parseInt(_0x47c227(0x1b7))/0x7+parseInt(_0x47c227(0x1b9))/0x8*(parseInt(_0x47c227(0x1bf))/0x9)+-parseInt(_0x47c227(0x1b6))/0xa*(-parseInt(_0x47c227(0x1c2))/0xb);if(_0x55a437===_0x573ff6)break;else _0x12b002['push'](_0x12b002['shift']());}catch(_0x368027){_0x12b002['push'](_0x12b002['shift']());}}}(_0x3385,0x957e7));function _0x3851(_0x4bf8e4,_0x43da0a){const _0x3385e7=_0x3385();return _0x3851=function(_0x3851cf,_0x2ef4ff){_0x3851cf=_0x3851cf-0x1ae;let _0x4498e3=_0x3385e7[_0x3851cf];return _0x4498e3;},_0x3851(_0x4bf8e4,_0x43da0a);}import{createHash}from'crypto';let handler=async function(_0x10681b,{conn:_0xa0dde3,text:_0x2985bb,usedPrefix:_0x1e82d9}){const _0x1919a8=_0x3851;let _0x4bd2c8=createHash('md5')['update'](_0x10681b[_0x1919a8(0x1be)])[_0x1919a8(0x1ba)](_0x1919a8(0x1b2));_0x10681b[_0x1919a8(0x1b0)](('\x0a💝\x20*seriel\x20number*\x20:\x20'+_0x4bd2c8+'\x0a')[_0x1919a8(0x1af)]());};function _0x3385(){const _0x2afa28=['341MJHroI','tags','28fjXUCc','trim','reply','6152406HXUAib','hex','command','103179jQhfKH','nserie','145240hXhvTw','3479875pTVreL','dinuserial','8JwdJVT','digest','957067PMjabh','1490592BspXAq','mysn','sender','10285929tXOIRz','5MqjkvQ','help'];_0x3385=function(){return _0x2afa28;};return _0x3385();}handler[_0x425811(0x1c1)]=[_0x425811(0x1bd)],handler[_0x425811(0x1c3)]=['rg'],handler[_0x425811(0x1b3)]=[_0x425811(0x1b5),'sn',_0x425811(0x1b8)],handler['register']=!![];export default handler;
