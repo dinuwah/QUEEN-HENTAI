0798cc29c8bef183909dbb05986112fc93cfa6d2
Dinuwa Official <119771445+dinuwah@users.noreply.github.com>
2023-06-18 18:35:36 +0530

    💝 QUEEN-HENTAI-donate.js

--- plugins/QUEEN-HENTAI-donate.js
+++ plugins/QUEEN-HENTAI-donate.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
     let don = `
 🇱🇰 *DONATE* 🇱🇰
 If you get more info of 💝 Queen Hentai 💝 Please Contact 94740804536 💰`
-let img = 'https://telegra.ph/file/7d84f663bc6e7fdac5c66'
+let img = 'https://telegra.ph/file/7d84f663bc6e7fdac5c66.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
