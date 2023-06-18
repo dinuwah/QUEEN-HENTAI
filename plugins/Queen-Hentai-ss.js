ec3d11d8dfa3f6cc780ecc2ade355283b3bafaa6
Dinuwa Official <119771445+dinuwah@users.noreply.github.com>
2023-06-18 18:30:50 +0530

    Queen-Hentai-ss.js

--- plugins/Queen-Hentai-ss.js
+++ plugins/Queen-Hentai-ss.js
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '*🇱🇰 Bza give a link or url to 💝 Queen Hentai 💝*', m)
-let ss = await (await fetch(`🇱🇰 https://image.thum.io/get/fullpage/${args[0]} 🇱🇰`)).buffer()
+if (!args[0]) return conn.reply(m.chat, '*🇱🇰 Bza give a link or url for Queen Hentai WA BOT*', m)
+let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], m)}
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['tools']
 handler.command = /^ss(web)?f?$/i
 export default handler
+
 
