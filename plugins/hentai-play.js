e28c68312bd84b1054f830e86fc864122b538877
GURU <sahoogurucharan00@gmail.com>
2023-07-18 19:31:46 +0530

    💝 Queen Hentai 💝
    play and ytv fix

--- plugins/hentai-play.js
+++ plugins/hentai-play.js
@@ -1,72 +1,84 @@
+import ytdl from 'ytdl-core';
+import yts from 'yt-search';
+import fs from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import os from 'os';
 
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import yts from 'yt-search'
+const streamPipeline = promisify(pipeline);
+
 var handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `Use example ${usedPrefix}${command} Queen Hentai`
-  await m.reply(wait)
-  let search = await yts(text)
-  let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
-  if (!search) throw 'Video Not Found, Try Another Title'
-  let { title, thumbnail, timestamp, views, ago, url } = vid
-  let wm = '*💝 Queen Hentai 💝* WA BOT'
+  if (!text) throw `Use example ${usedPrefix}${command} naruto blue bird`;
+  await m.react(rwait);
 
-  let captvid = `╭────  💝 Queen Hentai 💝 YT ─⬣
-💝 Title: ${title}
-💝 Duration: ${timestamp}
-💝 Views: ${views}
-💝 Upload: ${ago}
-💝 Link: ${url}
-╰────────⬣`
-  conn.sendButton(m.chat, `╭──── 💝 Queen Hentai 💝 YT ─⬣
-💝 Title: ${title}
-💝 Duration: ${timestamp}
-💝 Views: ${views}
-💝 Upload: ${ago}
-💝 Link: ${url}
-╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['💝 VIDEO 💝', `${usedPrefix}ytmp4 ${url}`], false, { quoted: m, 'document': { 'url':'💝 Queen Hentai 💝' },
-'mimetype': global.dpdf,
-'fileName': `💝 Queen Hentai 💝 ℙ𝕃𝔸𝕐𝔼ℝ`,
-'fileLength': 666666666666666,
-'pageCount': 666,contextInfo: { externalAdReply: { showAdAttribution: true,
-mediaType:  2,
-mediaUrl: `${url}`,
-title: `Song Is Uploading by 💝 Queen Hentai 💝...`,
-body: wm,
-sourceUrl: '💝 Queen Hentai 💝', thumbnail: await ( await conn.getFile(thumbnail)).data
-  }
- } 
-})
-  
-  //let buttons = [{ buttonText: { displayText: '💝 VIDEO 💝' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
- //let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
+  let search = await yts(text);
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
+  if (!search) throw 'Video Not Found, Try Another Title';
+  let { title, thumbnail, timestamp, views, ago, url } = vid;
+  let wm = '💝 Queen Hentai Downloading audio💝 please wait';
 
-  const yt = await youtubedlv2(url).catch(async _ => await youtubedl(url))
-const link = await yt.audio['128kbps'].download()
-  let doc = { 
-  audio: 
-  { 
-    url: link 
-}, 
-mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
-mediaType:  2,
-mediaUrl: url,
-title: title,
-body: wm,
-sourceUrl: url,
-thumbnail: await(await conn.getFile(thumbnail)).data                                                                     
-                                                                                                                 }
-                       }
-  }
+  let captvid = `✼ ••๑⋯ ❀ 💝 Queen Hentai 💝 ❀ ⋯⋅๑•• ✼
+  💝 Title: ${title}
+  💝 Duration: ${timestamp}
+  💝 Views: ${views}
+  💝 Upload: ${ago}
+  💝 Link: ${url}
+⊱─━━━━⊱༻●༺⊰━━━━─⊰`;
 
-  return conn.sendMessage(m.chat, doc, { quoted: m })
-	// return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
-	// return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
-}
-handler.help = ['play'].map(v => v + ' <query>')
-handler.tags = ['downloader']
-handler.command = /^(play|song)$/i
+  conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
 
-handler.exp = 0
-handler.diamond = false
 
-export default handler
+  const audioStream = ytdl(url, {
+    filter: 'audioonly',
+    quality: 'highestaudio',
+  });
+
+  // Get the path to the system's temporary directory
+  const tmpDir = os.tmpdir();
+
+  // Create writable stream in the temporary directory
+  const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
+
+  // Start the download
+  await streamPipeline(audioStream, writableStream);
+
+  let doc = {
+    audio: {
+      url: `${tmpDir}/${title}.mp3`
+    },
+    mimetype: 'audio/mp4',
+    fileName: `${title}`,
+    contextInfo: {
+      externalAdReply: {
+        showAdAttribution: true,
+        mediaType: 2,
+        mediaUrl: url,
+        title: title,
+        body: wm,
+        sourceUrl: url,
+        thumbnail: await (await conn.getFile(thumbnail)).data
+      }
+    }
+  };
+
+  await conn.sendMessage(m.chat, doc, { quoted: m });
+
+  // Delete the audio file
+  fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
+    if (err) {
+      console.error(`Failed to delete audio file: ${err}`);
+    } else {
+      console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
+    }
+  });
+};
+
+handler.help = ['play'].map((v) => v + ' <query>');
+handler.tags = ['downloader'];
+handler.command = /^play$/i;
+
+handler.exp = 0;
+handler.diamond = false;
+
+export default handler;
+
