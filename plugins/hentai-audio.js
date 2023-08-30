df8e0e937bc7aa457a42fc7fd29887cf0e9234de
aniwakdhdh <115075468+aniwakdhdh@users.noreply.github.com>
2023-08-30 13:32:03 +0530

    Update hentai-audio.js

--- plugins/hentai-audio.js
+++ plugins/hentai-audio.js
@@ -1,15 +1,14 @@
 let handler = m => m
 handler.all = async function (m) {
 
-    if (/^.alive$/i.test(m.text) ) {
+    if (/^.a$/i.test(m.text) ) {
       let av = './src/mp3/menu.mp3'
       this.sendPresenceUpdate('recording', m.chat);
       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
       }
 
-  if (/^.menu$/i.test(m.text) ) {
+  if (/^.hi|.gm|.alive|.menu$/i.test(m.text) ) {
     let av = './src/mp3/menu.mp3'
-    this.sendPresenceUpdate('recording', m.chat);
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
