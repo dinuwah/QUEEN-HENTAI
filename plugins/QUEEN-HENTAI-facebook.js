24d8d2346f5196ca7f48c950193f09521597b559
Dinuwa Official <119771445+dinuwah@users.noreply.github.com>
2023-08-30 15:00:54 +0530

    Update QUEEN-HENTAI-facebook.js

--- plugins/QUEEN-HENTAI-facebook.js
+++ plugins/QUEEN-HENTAI-facebook.js
@@ -1,1 +1,39 @@
-const _0x31570a=_0x4299;(function(_0x1953b3,_0x46a41d){const _0x57e8cf=_0x4299,_0x25138d=_0x1953b3();while(!![]){try{const _0x2a8db0=-parseInt(_0x57e8cf(0x1ea))/0x1+-parseInt(_0x57e8cf(0x1e1))/0x2*(parseInt(_0x57e8cf(0x1e8))/0x3)+parseInt(_0x57e8cf(0x1f6))/0x4+-parseInt(_0x57e8cf(0x1fd))/0x5+parseInt(_0x57e8cf(0x1f2))/0x6*(-parseInt(_0x57e8cf(0x1e9))/0x7)+-parseInt(_0x57e8cf(0x1ef))/0x8*(parseInt(_0x57e8cf(0x1ed))/0x9)+parseInt(_0x57e8cf(0x1e2))/0xa*(parseInt(_0x57e8cf(0x1e5))/0xb);if(_0x2a8db0===_0x46a41d)break;else _0x25138d['push'](_0x25138d['shift']());}catch(_0x4a12dc){_0x25138d['push'](_0x25138d['shift']());}}}(_0xd8d8,0x6acf7));import _0x556937 from'api-dylux';function _0xd8d8(){const _0xa2bf7c=['log','reverse','71238VONliJ','996779vPhRDe','611065jWbPsA','chat','&url=','897849EByjMS','\x20https://fb.watch/fOTpgn6UFQ/','8niEjHB','json','catch','12bsEEnK','download','*Generated\x20BY\x20💝\x20Queen\x20Hentai\x20💝','mp4','1551128XWvrGl','match','ERROR\x20WE\x20FIX\x20IT,💝\x20Queen\x20Hentai\x20💝','https://latam-api.vercel.app/api/facebookdl?apikey=brunosobrino&q=','data','result','bin','1438060PQrEnE','error.mp4','video','*💝\x20Queen\x20Hentai\x20💝\x20DOWNLOADING\x20YOUR\x20VIDEO\x20SOON..*','url','command','fbdl','32OVygKY','10bIqDqL','https://api.lolhuman.xyz/api/facebook?apikey=','sendFile','18841691HCmxGD'];_0xd8d8=function(){return _0xa2bf7c;};return _0xd8d8();}import _0x59ece0 from'node-fetch';import{savefrom,facebookdl,facebookdlv2}from'@bochilteam/scraper';import _0x533106 from'fb-downloader-scrapper';function _0x4299(_0x23d10,_0x327e8d){const _0xd8d81b=_0xd8d8();return _0x4299=function(_0x42991a,_0x1c6732){_0x42991a=_0x42991a-0x1e0;let _0x40bcb3=_0xd8d81b[_0x42991a];return _0x40bcb3;},_0x4299(_0x23d10,_0x327e8d);}let handler=async(_0x1f5466,{conn:_0x4dea1f,args:_0x50cfe9,command:_0x560cc0,usedPrefix:_0x44e827})=>{const _0x37d61c=_0x4299;if(!_0x50cfe9[0x0])throw'FOR\x20DOWNLOAD\x20FB\x20VIDEOS\x20FROM\x20💝\x20Queen\x20Hentai\x20💝,\x20EXAMPLE:\x20'+(_0x44e827+_0x560cc0)+_0x37d61c(0x1ee);if(!_0x50cfe9[0x0][_0x37d61c(0x1f7)](/www.facebook.com|fb.watch/g))throw'*NOT\x20A\x20FB\x20LINK\x20ID\x20BY\x20💝\x20Queen\x20Hentai\x20💝,\x20EXAMPLE:\x20'+(_0x44e827+_0x560cc0)+_0x37d61c(0x1ee);try{_0x1f5466['reply'](_0x37d61c(0x200));let _0x35917d=await _0x59ece0(_0x37d61c(0x1e3)+lolkeysapi+_0x37d61c(0x1ec)+_0x50cfe9[0x0]),_0x4060e5=await _0x35917d[_0x37d61c(0x1f0)](),_0x329a0d=_0x4060e5[_0x37d61c(0x1fb)][0x0];if(_0x329a0d==''||!_0x329a0d||_0x329a0d==null)_0x329a0d=_0x4060e5[_0x37d61c(0x1fb)][0x1];_0x4dea1f[_0x37d61c(0x1e4)](_0x1f5466['chat'],_0x329a0d,_0x37d61c(0x1fe),_0x37d61c(0x1f4),_0x1f5466);}catch(_0x3a32bf){console[_0x37d61c(0x1e6)]('1\x20'+_0x3a32bf);try{let _0xae7026=await _0x556937[_0x37d61c(0x1e0)](_0x50cfe9[0x0]),_0x81b77f=await _0xae7026[_0x37d61c(0x1fa)][0x0]['url'];await _0x4dea1f[_0x37d61c(0x1e4)](_0x1f5466[_0x37d61c(0x1eb)],_0x81b77f,_0x37d61c(0x1fe),_0x37d61c(0x1f4),_0x1f5466);}catch(_0x5c425f){console[_0x37d61c(0x1e6)]('2\x20'+_0x5c425f);try{let _0x275c4b=await _0x533106(_0x50cfe9[0x0]);for(let _0x5239b0 of _0x275c4b[_0x37d61c(0x1f3)]){let _0x4ee45e=_0x5239b0['url'];await _0x4dea1f[_0x37d61c(0x1e4)](_0x1f5466[_0x37d61c(0x1eb)],_0x4ee45e,_0x37d61c(0x1fe),'*Generated\x20BY\x20💝\x20Queen\x20Hentai\x20💝*',_0x1f5466);}}catch(_0xddb5ef){console[_0x37d61c(0x1e6)]('3\x20'+_0xddb5ef);try{let _0x3c42d3=await _0x59ece0('https://api.violetics.pw/api/downloader/facebook?apikey=beta&url='+_0x50cfe9[0x0]),_0x511030=await _0x3c42d3[_0x37d61c(0x1f0)](),_0x2416ea=''+(_0x511030['result']['hd'][_0x37d61c(0x201)]||_0x511030[_0x37d61c(0x1fb)]['sd'][_0x37d61c(0x201)]);await _0x4dea1f['sendFile'](_0x1f5466[_0x37d61c(0x1eb)],_0x2416ea,_0x37d61c(0x1fe),_0x37d61c(0x1f4),_0x1f5466);}catch(_0x420ce7){console[_0x37d61c(0x1e6)]('4\x20'+_0x420ce7);try{let _0x328270=await _0x59ece0(_0x37d61c(0x1f9)+_0x50cfe9[0x0]),_0x2864fe=await _0x328270[_0x37d61c(0x1f0)](),_0x408e7e=await _0x2864fe[_0x37d61c(0x1ff)];await _0x4dea1f['sendFile'](_0x1f5466['chat'],_0x408e7e,'error.mp4',_0x37d61c(0x1f4),_0x1f5466);}catch(_0x4721df){console[_0x37d61c(0x1e6)]('5\x20'+_0x4721df);try{const {result:_0x7963e8}=await facebookdl(_0x50cfe9[0x0])[_0x37d61c(0x1f1)](async _0x48b8d2=>await facebookdlv2(_0x50cfe9[0x0]))[_0x37d61c(0x1f1)](async _0x2df0d5=>await savefrom(_0x50cfe9[0x0]));for(const {url:_0x163ceb,isVideo:_0x24413a}of _0x7963e8[_0x37d61c(0x1e7)]())await _0x4dea1f['sendFile'](_0x1f5466[_0x37d61c(0x1eb)],_0x163ceb,'facebook.'+(!_0x24413a?_0x37d61c(0x1fc):_0x37d61c(0x1f5)),_0x37d61c(0x1f4),_0x1f5466);}catch(_0x2406c6){console[_0x37d61c(0x1e6)]('6\x20'+_0x2406c6);throw _0x37d61c(0x1f8);}}}}}}};handler[_0x31570a(0x202)]=/^(facebook|fb|facebookdl|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;export default handler;
+import fg from 'api-dylux';
+
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) {
+    throw `👸 Please send the link of a Facebook video\n\n👸 EX:\n*${usedPrefix + command}* https://www.facebook.com/satisfy2022/videos/1096430881329744/?d=null&vh=e`;
+  }
+
+  const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+  if (!urlRegex.test(args[0])) {
+    throw '👸 *PLEASE GIVE A VALID URL.*';
+  }
+
+  m.react(rwait);
+
+  try {
+    const result = await fg.fbdl(args[0]);
+    const tex = `
+👸─── {*💝 Queen Hentai 💝*} ─── ⊰
+👸 *VIDEO TITLE:* ${result.title}
+👸 ────── {⋆♬⋆} ────── ⊰`;
+
+    const response = await fetch(result.videoUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    const videoBuffer = Buffer.from(arrayBuffer);
+
+    conn.sendFile(m.chat, videoBuffer, 'fb.mp4', tex, m);
+    m.react(done);
+  } catch (error) {
+    console.log(error);
+    m.reply('👸 *An error.Try again later.*');
+  }
+};
+
+handler.help = ['facebook <url>'];
+handler.tags = ['dl'];
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i;
+handler.diamond = true;
+
+export default handler;
