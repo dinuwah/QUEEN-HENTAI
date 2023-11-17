5533076eefa9a5ee6a797ca4f92aa199b6ff4c5e
Tamoom <147646965+Tamoom@users.noreply.github.com>
2023-11-17 05:06:16 -0800

    Update config.js

--- config.js
+++ config.js
@@ -47,9 +47,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '💝 Queen Hentai 💝'
+global.botname = '💝 𝗧𝗮𝗶𝗳𝘂𝗿 𝗟𝘂𝗰𝗰𝗶 💝'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
+global.packname = '💝 𝗧𝗮𝗶𝗳𝘂𝗿 𝗟𝘂𝗰𝗰𝗶 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
 global.author = '💝 Dinuwa Official²⁰²³ 💝' 
 global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
 global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
@@ -59,7 +59,7 @@ global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
 global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
 
 
-global.wait = '*💝 _Waiting Bro Uploading by 💝 Queen Hentai 💝..._*'
+global.wait = '*💝 _Waiting Bro Uploading by 💝 𝗧𝗮𝗶𝗳𝘂𝗿 𝗟𝘂𝗰𝗰𝗶 💝..._*'
 global.rwait = '🇱🇰'
 global.dmoji = '🤣'
 global.done = '👍'
