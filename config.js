93f00d06613922a3047b9cb84787d4c3b602f97a
manuxtrigger <159012419+manuxtrigger@users.noreply.github.com>
2024-02-11 17:24:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ import axios from 'axios'
 
 global.owner = [
   ['+17633550598', 'manux Official²⁰²³', true],
-  ['+17633550598', 'manuxOfficial²⁰²³', true], 
+  ['+17633550598', 'manux Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['+17633550598','+17633550598'] 
-global.prems = ['+17633550598', '+17633550598', '+17633550598']
+global.mods = ['17633550598','17633550598'] 
+global.prems = ['17633550598', '17633550598', '17633550598']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
